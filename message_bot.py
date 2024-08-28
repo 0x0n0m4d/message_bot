3313f745d9e717e01356a64384b436abb991dc4d
d3m0n <th3l1ghtd3m0n@gmail.com>
2024-08-28 19:10:03 -0300

    handle errors

--- message_bot.py
+++ message_bot.py
@@ -2,23 +2,19 @@ import re
 import json
 import time
 import requests
-from random import randrange
+import pywhatkit
 from tqdm import tqdm
-
-# to simulate storage variables
-clients = []
-
-# Add a tag to client number
-def add_tag(num):
-    # @todo: create a function to add tags called 'Jhony' to numbers
-    return num
-
+import webbrowser as web
+from random import randrange
 
 # get cookies from request har file
 def get_cookies():
     # read HAR file
-    with open("request.har", "r") as file:
-        har_data = json.load(file)
+    try:
+        with open("request.har", "r") as file:
+            har_data = json.load(file)
+    except:
+        raise NameError('\x1b[31m ERROR:\x1b[0m Missing \x1b[34m\x1b[1mrequest.har\x1b[0m file!')
 
     # extract cookies
     har_cookies = har_data["log"]["entries"][0]["request"]["cookies"]
@@ -31,19 +27,20 @@ def get_cookies():
     for cookie in har_cookies:
         if cookie["name"] == "session_id":
             if cookie["value"] == "":
-                raise NameError('Missing Important Cookies!')
+                raise NameError('\x1b[31m ERROR:\x1b[0m Missing Important Cookies In File!')
             else:
                 res["session_id"] = cookie["value"]
 
         if cookie["name"] == "cactk":
             if cookie["value"] == "":
-                raise NameError('Missing Important Cookies!')
+                raise NameError('\x1b[31m ERROR:\x1b[0m Missing Important Cookies In File!')
             else:
                 res["cactk"] = cookie["value"]
 
     return res
 
 
+# Get ids in api to search numbers
 def get_ids(link, cookies, headers):
     response = requests.get(link, cookies=cookies, headers=headers, timeout=10)
     time.sleep(3)
@@ -81,6 +78,8 @@ def get_clients():
         "User-Agent": "Mozilla/5.0",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"
     }
+    clients = []
+
     for page in range(1, 1000):
         link = "https://www.catho.com.br/curriculos/busca/?q=vendas&pais_id=31&estado_id[25]=25&regiaoId[-1]=-1&cidade_id[783]=783&zona_id[-1]=-1&page="+ str(page) +"&onde_buscar=todo_curriculo&como_buscar=todas_palavras&tipoBusca=busca_palavra_chave&idade[1]=1&empregado=false&dataAtualizacao=30&buscaLogSentencePai=111e5bd0-8cee-4ed4-8ff3-51f9669f13f3"
         data = get_ids(link, cookies, headers)
@@ -91,9 +90,9 @@ def get_clients():
         if len(infos) == 0:
             break
 
-        print(f'\x1b[34m[󰀖]\x1b[0m Gathering clients data in page \x1b[1m{page}\x1b[0m!!!')
-        # add cv_id && usr_id in memory
-        for client in tqdm(infos, desc="[\x1b[32m"+str(page)+"\x1b[0m]"):
+        print(f'\x1b[34m\x1b[0m Gathering clients data in page:')
+
+        for client in tqdm(infos, desc="\x1b[32m\x1b[1m"+str(page)+"\x1b[0m 󰁕"):
             usr_id = client["usr_id"]
             cv_id = client["cv_id"]
             number = get_number(link, cv_id, usr_id, hashPage)
@@ -108,21 +107,51 @@ def get_clients():
 
 
     print("[+] \x1b[32m" + str(len(clients)) + "\x1b[0m ids collecteds!")
+
+    # if `database.json` exist, the file will be overriden
     with open('database.json', 'w') as fout:
         json.dump(clients, fout)
 
 
-# send messages to all users in the database
-# database with numbers should exist
-def send_messages():
-    add_tag("")
-    # @todo: create a function to send messages
-    # @> messages need be send in random times between 1s and 20s
+# Add a tag to client number
+def send_message(num):
+
+    return True
+
+
+# handle messages to send text to all users in the database
+def handle_messages():
+    counter = 0
+    # check if database exists
+    try:
+        with open("database.json", "r") as f:
+            data = json.load(f)
+    except:
+        raise NameError('\x1b[31m ERROR:\x1b[0m Missing \x1b[34m\x1b[1mdatabase.json\x1b[0m file!')
+
+    for client in data:
+        if counter >= 200:
+            break
+
+        # formatting and send message to numbers
+        for l in client["number"]:
+            f_num = "+55"
+            if l == "(" or \
+               l == ")" or \
+               l == " " or \
+               l == "-":
+                f_num = f_num + ""
+            else:
+                f_num = f_num + l
+                if send_message(num):
+                    counter += 1
+                else:
+                    continue
 
 
 def main():
     get_clients()
-    send_messages()
+    handle_messages()
 
 
 if __name__ == "__main__":
