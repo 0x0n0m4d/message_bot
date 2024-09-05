02864d672b0fa031c65f3e6ef1bd5448d8059ffb
d3m0n <th3l1ghtd3m0n@gmail.com>
2024-09-05 16:46:06 -0300

    fix typo

--- message_bot.py
+++ message_bot.py
@@ -110,7 +110,7 @@ def get_clients():
     cookies = get_cookies()
     headers = {
         "User-Agent": "Mozilla/5.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "x-auth-token": cookies["cactk"]
     }
     clients = []
